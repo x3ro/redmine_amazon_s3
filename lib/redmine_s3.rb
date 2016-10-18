473c537dc1776a0ee8e05e4d6e61840e462239d6
Lucas Jenss <lucas@x3ro.de>
2016-10-18 16:11:09 +0200

    Rename directory to match current plugin name

--- lib/redmine_s3.rb
+++ lib/redmine_s3.rb
@@ -1,9 +1,9 @@
-require 'redmine_s3/patches/attachment_patch'
-require 'redmine_s3/patches/attachments_controller_patch'
-require 'redmine_s3/patches/application_helper_patch'
-require 'redmine_s3/thumbnail'
-require 'redmine_s3/configuration'
-require 'redmine_s3/connection'
+require 'amazon_s3/patches/attachment_patch'
+require 'amazon_s3/patches/attachments_controller_patch'
+require 'amazon_s3/patches/application_helper_patch'
+require 'amazon_s3/thumbnail'
+require 'amazon_s3/configuration'
+require 'amazon_s3/connection'
 
 AttachmentsController.send(:include, RedmineS3::AttachmentsControllerPatch)
 Attachment.send(:include, RedmineS3::AttachmentPatch)
